62f6f88ab0161685b7297daadb572cda841fc4e9
McloiGG <marcusloiminghan@gmail.com>
2026-02-03 16:50:56 +0800

    feature: unbounded areas beyound pmap also rendered as unbounded in minimap

--- SRCS/parsing/init_gamedata.c
+++ SRCS/parsing/init_gamedata.c
@@ -6,7 +6,7 @@
 /*   By: lming-ha <lming-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 01:51:14 by jchuah            #+#    #+#             */
-/*   Updated: 2026/02/03 11:33:42 by lming-ha         ###   ########.fr       */
+/*   Updated: 2026/02/03 16:46:21 by lming-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,47 @@
 #include "parsing.h"
 #include "rendering.h"
 
-static void	set_wall_cell(t_gamedata *gamedata, int x, int y, int t_idx)
+static void	set_map_cell(t_gamedata *gamedata, t_map *pmap, int **mask, int x, int y)
 {
-	if (gamedata->pmap.content[y][x] == 'D')
-		gamedata->map[y][x].e_type = DOOR;
-	else
-		gamedata->map[y][x].e_type = WALL;
-	gamedata->map[y][x].north = &gamedata->texture_pack.texture[t_idx][0];
-	gamedata->map[y][x].south = &gamedata->texture_pack.texture[t_idx][1];
-	gamedata->map[y][x].west = &gamedata->texture_pack.texture[t_idx][2];
-	gamedata->map[y][x].east = &gamedata->texture_pack.texture[t_idx][3];
-	gamedata->map[y][x].solid = 1;
-	gamedata->map[y][x].visible = 1;
+	int	t_idx;
+
+	t_idx = get_wall(pmap->content[y][x]);
+	if (pmap->content[y][x] == ' ' && mask[y][x] == 3)
+		gamedata->map[y][x].e_type = UNBOUND;
+	else if (ft_strchr(" 0", pmap->content[y][x]))
+		gamedata->map[y][x].e_type = EMPTY;
+	if (pmap->content[y][x] == 'D' || t_idx)
+	{
+		if (gamedata->pmap.content[y][x] == 'D')
+			gamedata->map[y][x].e_type = DOOR;
+		else
+			gamedata->map[y][x].e_type = WALL;
+		gamedata->map[y][x].north = &gamedata->texture_pack.texture[t_idx][0];
+		gamedata->map[y][x].south = &gamedata->texture_pack.texture[t_idx][1];
+		gamedata->map[y][x].west = &gamedata->texture_pack.texture[t_idx][2];
+		gamedata->map[y][x].east = &gamedata->texture_pack.texture[t_idx][3];
+		gamedata->map[y][x].solid = 1;
+		gamedata->map[y][x].visible = 1;
+	}
+	if (pmap->content[y][x] == 'C')
+		add_coin(gamedata, &gamedata->coins, x, y);
 }
 
 static void	arr_to_map_cell(t_gamedata *gamedata, t_map *pmap, int **mask)
 {
-	int		y;
-	int		x;
-	int		t_idx;
+	int	y;
+	int	x;
 
 	y = 0;
-	while (y < pmap->height)
+	while (y < MAP_SIZE_MAX)
 	{
 		x = 0;
-		while (x < pmap->width && pmap->content[y][x])
+		while (x < MAP_SIZE_MAX)
 		{
-			t_idx = get_wall(pmap->content[y][x]);
-			if (pmap->content[y][x] == ' ' && mask[y][x] == 3)
+			if (y < pmap->height && x < pmap->width)
+				set_map_cell(gamedata, pmap, mask, x, y);
+			else
 				gamedata->map[y][x].e_type = UNBOUND;
-			else if (ft_strchr(" 0", pmap->content[y][x]))
-				gamedata->map[y][x].e_type = EMPTY;
-			if (pmap->content[y][x] == 'D' || t_idx)
-				set_wall_cell(gamedata, x, y, t_idx);
-			if (pmap->content[y][x] == 'C')
-				add_coin(gamedata, &gamedata->coins, x, y);
 			x++;
 		}
 		y++;
